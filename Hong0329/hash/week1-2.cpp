bddb28bb7c8e063529a51c4cb89c919591b7200d
Hong0329 <rhkgkrwkhj@naver.com>
2024-02-15 04:09:08 +0900

    week1-2

--- Hong0329/hash/week1-2.cpp
+++ Hong0329/hash/week1-2.cpp
@@ -1,20 +1,33 @@
 //
-// Created by hong jun chung on 2/15/24.
+// https://school.programmers.co.kr/learn/courses/30/lessons/42576
 //
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
 
-string solution(vector<string> participant, vector<string> completion) {
+
+string solution(vector<string> participant, vector<string> completion)
+{
     string answer = "";
+    unordered_map<string, int> map;
 
-    sort(participant.begin(),participant.end());
-    sort(completion.begin(),completion.end());
+    for (int i = 0 ; i < participant.size() ; i++)
+    {
+        if (map.end() == map.find(participant[i]))
+            map.insert(make_pair(participant[i], 1));
+        else
+            map[participant[i]]++;
+    }
 
-    for(int i = 0 ; i < participant.size() ; i++) {
-        if(participant[i] != completion[i]) {
-            answer = participant[i];
+    for (int j = 0 ; j < completion.size() ; j++)
+        map[completion[j]]--;
+
+    for(int t = 0 ; t < participant.size() ; t++){
+        if (map[participant[t]] > 0)
+        {
+            answer = participant[t];
             break;
         }
     }
