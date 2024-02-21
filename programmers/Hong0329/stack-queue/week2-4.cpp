6aabbef6b5ba4967a5832c278fdffd6588b55471
Hong0329 <rhkgkrwkhj@naver.com>
2024-02-22 04:19:53 +0900

    week2-4

---
+++ programmers/Hong0329/stack-queue/week2-4.cpp
@@ -0,0 +1,44 @@
+//
+// Created by hong jun chung on 2/22/24.
+//
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b) {
+    if(a.first == b.first)
+        return a.second < b.second;
+    return a.first > b.first;
+}
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    int key = 0;
+    int cnt = 0;
+    vector<pair<int,int>>vec;
+    queue<pair<int,int>>que;
+
+    for(int i =0 ;i<priorities.size();i++){
+        vec.push_back(make_pair(priorities[i],i));
+        que.push(make_pair(priorities[i],i));
+    }
+
+    sort(vec.begin(),vec.end(),compare);
+
+    while(!que.empty()) {
+        while(vec[key].first!=que.front().first && vec[key].second!=que.front().second) {
+            que.push(que.front());
+            que.pop();
+        }
+        cnt++;
+        key++;
+        if(que.front().second == location){
+            answer = cnt;
+        }
+        que.pop();
+    }
+    return answer;
+}
