1c27338e4419d0b0389bb3355cbe8189ca6cb4cc
sung-silver <jinna0319@gmail.com>
2024-02-29 15:06:17 +0900

    [3주차] JAVA: 더 맵게

---
+++ programmers/sung-silver/heap/week3-1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i : scoville){
+            pq.add(i);
+        }
+        while(pq.peek() < K){
+            if(pq.size() == 1){
+                return -1;
+            }
+            int a = pq.poll();
+            int b = pq.poll();
+            pq.add(a + b * 2);
+            answer++;
+        }
+        return answer;
+    }
+}
