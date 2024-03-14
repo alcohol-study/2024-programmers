fc0c771b7df8911d6989e0a96bf81f0e6e578ce8
Hong0329 <rhkgkrwkhj@naver.com>
2024-03-14 20:07:38 +0900

    week5-5

---
+++ programmers/Hong0329/bf/week5-5.cpp
@@ -0,0 +1,30 @@
+//
+// https://school.programmers.co.kr/learn/courses/30/lessons/87946
+//
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, vector<vector<int>> dungeons) {
+    int answer = -1;
+
+    sort(dungeons.begin(), dungeons.end());
+
+    do{
+        int num = k;
+        int cnt = 0;
+
+        for(int i = 0; i< dungeons.size(); i++) {
+            if(dungeons[i][0] <= num) {
+                num -= dungeons[i][1];
+                cnt++;
+            }
+        }
+
+        answer = max(answer, cnt);
+    } while(next_permutation(dungeons.begin(), dungeons.end()));
+    return answer;
+}
