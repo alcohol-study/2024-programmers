32b8e254b4836e7cb23d0091128cffeac89bb3f3
Hong0329 <rhkgkrwkhj@naver.com>
2024-05-30 21:48:57 +0900

    week14-algo3

---
+++ programmers/algorithm/level2/Hong0329/짝지어 제거하기.cpp
@@ -0,0 +1,22 @@
+//
+// Created by hong jun chung on 5/30/24.
+//
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s) {
+    stack<char> str;
+    for (int i = 0; i < s.length(); i++) {
+        if (str.empty() || str.top() != s[i])
+            str.push(s[i]);
+
+        else if (str.top() == s[i])
+            str.pop();
+    }
+
+    if (str.empty())
+        return 1;
+    return 0;
+}
