778c5b465884382fecb80ebe9a8f2b397fce786d
hellozo0 <ahju0011@gmail.com>
2024-06-06 19:42:24 +0900

    [15주차] JAVA : 카펫

---
+++ programmers/algorithm/level2/hellozo0/week15/카펫.java
@@ -0,0 +1,32 @@
+package programmers.algorithm.level2.hellozo0.week15;
+
+public class 카펫 {
+import java.util.*;
+
+    class Solution {
+        public int[] solution(int brown, int yellow) {
+
+            int[] answer = new int[2];
+            int sum = brown + yellow;
+
+            for (int i = 3; i < sum; i++) {
+                int j = sum / i;
+
+                if (sum % i == 0 && j >= 3) {
+                    int col = Math.max(i, j); //이부분 이전과는 다른
+                    int row = Math.min(i, j);
+                    int center = (col - 2) * (row - 2);
+
+                    if (center == yellow) {
+                        answer[0] = col;
+                        answer[1] = row;
+                        return answer;
+                    }
+                }
+            }
+            return answer;
+
+        }
+    }
+
+}
