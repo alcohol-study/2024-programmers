4d19ed91b0ea4e2520feb8e04284b664d8952f9a
hellozo0 <ahju0011@gmail.com>
2024-06-06 19:58:27 +0900

    [15주차] JAVA : N개의 최소공배수

---
+++ programmers/algorithm/level2/hellozo0/week15/N개의_최소공배수.java
@@ -0,0 +1,28 @@
+package programmers.algorithm.level2.hellozo0.week15;
+
+public class N개의_최소공배수 {
+    class Solution {
+        public int solution(int[] arr) {
+            int answer = arr[0];
+            int g = 0;
+
+            if(arr.length > 1) {
+                for(int i = 1; i < arr.length; i++) {
+                    g = gcd(answer, arr[i]);
+                    answer = (answer * arr[i]) / g;
+                }
+            }
+
+            return answer;
+        }
+
+        //최대 공약수 구하는 함수
+        private static int gcd(int a, int b) {
+            int num = a % b;
+            if(num == 0) return b;
+            else return gcd(b, num);
+        }
+
+    }
+    }
+}
