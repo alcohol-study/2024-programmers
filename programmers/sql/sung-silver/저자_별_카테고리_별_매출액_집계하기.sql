0b6336a7ff439e5db42c0440f6bc7fd9ba9c010a
sung-silver <jinna0319@gmail.com>
2024-08-02 12:26:49 +0900

    [22주차] SQL: 저자 별 카테고리 별 매출액 집계하기

---
+++ programmers/sql/sung-silver/저자_별_카테고리_별_매출액_집계하기.sql
@@ -0,0 +1,6 @@
+SELECT a.author_id as AUTHOR_ID, a.author_name as AUTHOR_NAME, b.category as CATEGORY,
+SUM(bs.SALES * b.PRICE) as SALES
+FROM BOOK b JOIN AUTHOR a ON b.author_id = a.author_id JOIN BOOK_SALES bs on bs.book_id = b.book_id
+WHERE YEAR(bs.sales_date) = 2022 AND MONTH(bs.sales_date) = 1
+GROUP BY AUTHOR_ID, AUTHOR_NAME, CATEGORY
+ORDER BY AUTHOR_ID ASC, CATEGORY DESC;
