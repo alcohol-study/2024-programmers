f9a10efb1b3cabcadf77d8ce00c5ebf6d1e3152c
hellozo0 <ahju0011@gmail.com>
2024-08-16 23:03:19 +0900

    [24주차] JAVA : 단어 변환

---
+++ programmers/algorithm/level3/hellozo0/week24/단어변환.java
@@ -0,0 +1,43 @@
+package programmers.algorithm.level3.hellozo0.week24;
+
+public class 단어변환 {
+
+    class Solution {
+
+        static int answer;
+        static boolean visited[];
+
+        public int solution(String begin, String target, String[] words) {
+            answer = 0;
+            visited = new boolean[words.length];
+
+            dfs(begin, target, words, 0);
+            return answer;
+        }
+
+        public void dfs(String begin, String target, String[] words, int count){
+            if(begin.equals(target)){
+                answer = count;
+                return;
+            }
+
+            for(int i = 0; i < words.length; i++){
+                if(visited[i]) continue;
+                int check = 0;
+
+                for(int j = 0; j < begin.length(); j++){
+                    if(begin.charAt(j) == words[i].charAt(j)){
+                        check++;
+                    }
+
+                    if(check == begin.length() - 1){
+                        visited[i] = true;
+                        dfs(words[i], target, words, count+1);
+                        visited[i] = false;
+                    }
+                }
+            }
+        }
+    }
+
+}
