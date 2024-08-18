b0073f54f4515e5a92ba9bf9882913d7cb65dfb0
sung-silver <jinna0319@gmail.com>
2024-08-18 23:19:49 +0900

    [24주차] JAVA: 단어 변환

--- programmers/algorithm/level3/sung-silver/단어_변환.java
+++ programmers/algorithm/level3/sung-silver/단어_변환.java
@@ -1,35 +1,62 @@
-import java.util.*;
-
 class Solution {
+    int answer = 0;
+    boolean[] visited;
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = Integer.MAX_VALUE;
-        Set<String> wordSet = new HashSet<>();
-
-        for(String word : words){
-            wordSet.add(word);
-        }
-
-        if(!wordSet.contains(target)){
+        answer = Integer.MAX_VALUE;
+        
+        if(!contains(words, target)){
             return 0;
         }
-
-        dfs(begin, target, 1, wordSet, 1, answer);
-
-        return answer;
-    }
-
-    private void dfs(String compare, String target, int idx, Set<String> wordSet, int count, int answer){
-        System.out.println(compare);
-        if(compare.equals(target)){
-            answer = Math.min(count, answer);
-            return ;
-        }
-
-        for(char i ='a' ; i<'z' ;i++ ){
-            String newCompare = compare.substring(0, idx-1) + i + compare.substring(idx+1, compare.length());
-            if(wordSet.contains(newCompare)){
-                dfs(newCompare, target, idx+1, wordSet, count+1, answer);
+        
+        for (int i = 0; i < words.length; i++) {
+            if (canChange(begin, words[i])) {
+                visited = new boolean[words.length];
+                visited[i] = true;
+                dfs(1, target, words[i], words);
             }
         }
+        
+        return answer;
+    }
+    
+    private boolean contains(String[] words, String target){
+        for(String word : words){
+            if(word.equals(target)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private void dfs(int idx, String target, String current, String[] words){
+        if(target.equals(current)){
+            answer = Math.min(idx, answer);
+            return ;
+        }
+        
+        for (int i = 0; i < words.length; i++) {
+            if (!visited[i] && canChange(current, words[i])) {
+                visited[i] = true;
+                dfs(idx + 1, target, words[i], words);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    private static boolean canChange(String begin, String target) {
+        int notEqualSize = 0;
+
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) != target.charAt(i)) {
+                notEqualSize++;
+            }
+        }
+        
+        if(notEqualSize==1){
+            return true;
+        }
+
+        return false;
     }
 }
